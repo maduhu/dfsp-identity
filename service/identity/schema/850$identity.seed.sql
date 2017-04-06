34ce9a74a059ce9d38d5f9c5c8c82317793a0ed9
Ventsislav Dimitrov <ventsislav.dimitrov@softwaregroup-bg.com>
2017-04-06 15:09:40 +0300

    fix: remove account roles from identity roles

--- service/identity/schema/850$identity.seed.sql
+++ service/identity/schema/850$identity.seed.sql
@@ -53,15 +53,12 @@ ON CONFLICT ("name") DO UPDATE SET "description" = EXCLUDED.description;
 
 -- insert roles
 INSERT INTO
-   identity."role" ("name", "description", "isPublic")
+   identity."role" ("name", "description")
 VALUES
-  ('common', 'Default role', 'false'),
-  ('maker', 'Batch payment maker role', 'false'),
-  ('checker', 'Batch payment checker role', 'false'),
-  ('customer', 'USSD customer role', 'true'),
-  ('merchant', 'USSD merchant role', 'true'),
-  ('agent', 'USSD agent role', 'true')
-ON CONFLICT ("name") DO UPDATE SET "description" = EXCLUDED.description, "isPublic" = EXCLUDED."isPublic";
+  ('common', 'Default role'),
+  ('maker', 'Batch payment maker role'),
+  ('checker', 'Batch payment checker role')
+ON CONFLICT ("name") DO UPDATE SET "description" = EXCLUDED.description;
 
 INSERT INTO
    identity."roleAction" ("roleId", "actionId")
