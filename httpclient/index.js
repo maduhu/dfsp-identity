e37bdcd185a9520bd2966a8956bb2ac1993bde80
Radoslav Viktorov <Radoslav.viktorov@Softwaregroup-bg.com>
2017-07-13 18:33:32 +0300

    fix: add user data obfuscation configuration

--- httpclient/index.js
+++ httpclient/index.js
@@ -4,5 +4,15 @@ module.exports = {
   url: 'http://localhost:8010',
   namespace: ['directory'],
   logLevel: 'debug',
+  log: {
+    transform: {
+      payee: 'hide',
+      name: 'hide',
+      firstName: 'hide',
+      lastName: 'hide',
+      nationalId: 'hide',
+      dob: 'hide'
+    }
+  },
   method: 'post'
 }
